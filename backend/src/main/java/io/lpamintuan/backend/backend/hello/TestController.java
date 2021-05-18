d7909fe82d3cfb114b6cf452ec50c56983d1b975
Lyndonn Pamintuan <pamintuan.lyndonn0503@gmail.com>
2021-05-18 18:03:53 +0800

    adding  backend spring boot app

---
+++ backend/src/main/java/io/lpamintuan/backend/backend/hello/TestController.java
@@ -0,0 +1,13 @@
+package io.lpamintuan.backend.backend.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+}
